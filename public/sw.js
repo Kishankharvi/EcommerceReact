f180b0c81fbd055f406e31248daf5f349da5e8e0
Kishan S <98368606+Kishankharvi@users.noreply.github.com>
2025-06-24 23:09:27 +0530

    firebase code added

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = "nexus-store-v1"
 const urlsToCache = ["/", "/static/js/bundle.js", "/static/css/main.css", "/manifest.json"]
 
-// Install event
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -10,17 +10,16 @@ self.addEventListener("install", (event) => {
   )
 })
 
-// Fetch event
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Return cached version or fetch from network
+    
       return response || fetch(event.request)
     }),
   )
 })
 
-// Activate event
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
